ebbf008c2bb430c9cceef9a43b11c0ed606c1a73
Vio <vio@beanon.com>
2023-09-21 23:13:49 +0200

    build: Add rollup-plugin-bundle-stats

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { webpackStats } from 'rollup-plugin-webpack-stats';
+import { bundleStats } from 'rollup-plugin-bundle-stats';
 
 export default defineConfig((env) => ({
   plugins: [
     react(),
+    bundleStats(),
     // Output webpack-stats.json file
     webpackStats(),
   ],
