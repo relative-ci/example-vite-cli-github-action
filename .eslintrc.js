ec9380b6b390d15eb0f89a832b7a8a39c82c2509
Vio <vio@beanon.com>
2025-11-09 13:29:52 +0100

    build: Run lint check

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['airbnb'],
   rules: {
     'import/no-extraneous-dependencies': ['error', { devDependencies: ['*.config.js', '*.config.ts', '*.config.mjs'] }],
+    'import/prefer-default-export': 'off',
   },
 };
